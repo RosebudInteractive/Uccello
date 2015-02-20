898eb09374c18a6060fb1740316e2e892c021d94
Sergey <rudserg@gmail.com>
2015-02-20 16:18:40 +0300

    move session in db

--- connection/userSessionMgr.js
+++ connection/userSessionMgr.js
@@ -201,6 +201,8 @@ define(
                         if (userObj) {
                             that.removeUser(session.getUser().name());
                             session.getUser().name(data.name);
+                            userObj.getObj().getCol("Sessions")._add(session.getObj());
+                            session.getUser().getObj().getCol("Sessions")._del(session.getObj())
                         } else {
                             userObj = session.getUser();
                             that.removeUser(userObj.name());
