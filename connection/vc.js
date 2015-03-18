73f40179724419d64d578992592575765663f3f0
IvanCollector <ivan.collector@gmail.com>
2015-03-18 09:30:43 +0300

    2

--- connection/vc.js
+++ connection/vc.js
@@ -8,30 +8,107 @@ if (typeof define !== 'function') {
  * @module VisualContext2
  */
 define(
-    ['../system/umodule'],
-    function(UModule) {
+    ['./visualContextinfo'],
+    function(VisualContextInfo) {
 					 
-        var VisualContext2 = UModule.extend(/** @lends module:VisualContext2.VisualContext2.prototype */{
+        var VisualContext2 = VisualContextInfo.extend(/** @lends module:VisualContext2.VisualContext2.prototype */{
 
             className: "VisualContext2",
             classGuid: UCCELLO_CONFIG.classGuids.VisualContext2,
-            metaFields: [
-                {fname: "DataBase", ftype: "string"}, // runtime
-				{fname: "Kind", ftype: "string"}, // , fdefault: "master" enum (master,slave
-				{fname: "MasterGuid", ftype: "string"},
-				{fname: "ContextGuid", ftype: "string"}
-            ],
-            metaCols: [],
+            metaFields: [],
 
              /**
              * Инициализация объекта
              * @constructs
              * @param params {object} 
-			 * @callback cb - коллбэк, который вызывается после отработки конструктора (асинхронный в случае slave)
              */
             init: function(cm, params,cb) {
-                this._super(cm, params);
+                this._super(cm, params, cb);
             },
+			
+			// включить контекст
+			on: function(cm, params,cb) {
+				this.pvt.cmgs = {};
+				this.pvt.db = null;
+				this.pvt.tranQueue = null; // очередь выполнения методов если в транзакции
+				this.pvt.inTran = false; // признак транзакции
+
+                if (params == undefined) return;
+				
+				this.pvt.typeGuids = params.typeGuids;
+				var controller = cm.getDB().getController();
+				this.pvt.rpc = params.rpc;
+				this.pvt.proxyServer = params.proxyServer;
+				this.pvt.components = params.components;
+				this.pvt.config = params.config;
+				this.pvt.renderRoot = params.renderRoot;
+				this.pvt.formParams = {};
+				this.pvt.memParams = [];
+
+				var that = this;	
+				var createCompCallback = null;
+				if (cb)
+					createCompCallback = function (obj) {
+						var rootGuid = obj.getRoot().getGuid();
+						if (!(that.pvt.cmgs[rootGuid]))
+							that.pvt.cmgs[rootGuid] = new ControlMgr(that.getDB(),rootGuid,that);
+						that.createComponent.apply(that, [obj, that.pvt.cmgs[rootGuid]]);						
+						 
+					}
+				else // пока что считаем, что если нет финального колбэка - мы на сервере
+					createCompCallback = function (obj) { 
+						if (obj.getTypeGuid() == UCCELLO_CONFIG.classGuids.FormParam) { // Form Param
+							obj.event.on({ 
+								type: "mod", // TODO не забыть про отписку
+								subscriber: that,
+								callback: that._onModifParam
+							});
+							
+							if (!that.pvt.formParams[obj.get("Name")])  // добавить в список параметров
+								that.pvt.formParams[obj.get("Name")] = [];
+							that.pvt.formParams[obj.get("Name")].push(obj);
+						
+						}
+						// подписаться на событие завершения applyDelta в контроллере, чтобы переприсвоить параметры 
+						controller.event.on({
+							type: 'end2ApplyDeltas',
+							subscriber: that,
+							callback: that._setFormParams
+						});					
+					}
+					
+				if (this.kind()=="master") { // главная (master)
+					this.pvt.vcproxy = params.rpc._publ(this, Interfvc);
+					var params2 = {name: "VisualContextDB", kind: "master", cbfinal:cb};
+					if (createCompCallback)
+						params2.compcb = createCompCallback;
+					this.pvt.db = this.createDb(controller,params2);
+					this.loadNewRoots(params.formGuids, { rtype: "res", compcb: params2.compcb},params2.cbfinal);
+					this.dataBase(this.pvt.db.getGuid());
+				}
+				else { // подписка (slave)
+				
+					this.pvt.vcproxy = params.rpc._publProxy(params.vc, params.socket,Interfvc);
+					var guid = this.masterGuid();
+
+					this.pvt.db = controller.newDataBase({name:"Slave"+guid, proxyMaster : { connect: params.socket, guid: guid}}, function(){
+                            // подписываемся либо на все руты либо выборочно formGuids
+							var forms = params.formGuids;
+							if (forms == null) forms = "all";
+							else if (forms == "") forms = [];
+                            that.getDB().subscribeRoots(forms, cb, createCompCallback);
+							that.dataBase(that.getDB().getGuid());
+						});
+				}
+				this.pvt.db.setDefaultCompCallback(createCompCallback);
+
+				this.contextGuid(this.getGuid());			
+			},
+			
+			// выключить контекст
+			off: function() {
+			
+			},
 
             dataBase: function (value) {
                 return this._genericSetter("DataBase", value);
