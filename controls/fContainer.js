308e60018720dd832813e7ed79810c913b7df0a6
LevanKiknadze <levan@rosebud.com>
2015-05-26 13:44:26 +0400

    fContainer

--- controls/fContainer.js
+++ controls/fContainer.js
@@ -11,12 +11,31 @@ define(
             className: "FContainer",
             classGuid: UCCELLO_CONFIG.classGuids.FContainer,
             metaCols: [{"cname": "Children", "ctype": "control"}],
-            metaFields: [],
+            metaFields: [
+                {fname:"HasGrid", ftype:"boolean"},
+                {fname:"ColumnsCount", ftype:"int"},
+                {fname:"MaxColWidth", ftype:"int"},
+                {fname:"MinColWidth", ftype:"int"}
+            ],
 
             init: function(cm,params){
                 this._super(cm,params);
+            },
+            hasGrid: function(value) {
+                return this._genericSetter("HasGrid", value);
+            },
+            columnsCount: function(value) {
+                return this._genericSetter("ColumnsCount", value);
+            },
+            maxColWidth: function(value) {
+                return this._genericSetter("MaxColWidth", value);
+            },
+            minColWidth: function(value) {
+                return this._genericSetter("MinColWidth", value);
             }
         });
+
+
         return FContainer;
     }
 );
