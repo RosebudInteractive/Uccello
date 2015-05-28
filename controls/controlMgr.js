a5efff6db6a3b857fb14a6f6c270c8832f130bfa
IvanCollector <ivan.collector@gmail.com>
2015-05-28 09:43:13 +0300

    x

--- controls/controlMgr.js
+++ controls/controlMgr.js
@@ -4,8 +4,8 @@
 }
 
 define(
-	['../memDB/memDataBase', './viewset'],
-	function(MemDataBase, ViewSet) {
+	['../memDB/memDataBase', './viewset','../system/Utils'],
+	function(MemDataBase, ViewSet,Utils) {
 		var ControlMgr = MemDataBase.extend({
 
             /**
